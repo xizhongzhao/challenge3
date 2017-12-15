e338ae39f3827e6f7119c38745191678d03d6a73
hades_zhao <hades_zhao@126.com>
2017-12-15 13:53:05 +0800

    modify class Config Userdata add @property to userdata config

--- calculator.py
+++ calculator.py
@@ -3,36 +3,33 @@
 class Config(object):
     def __init__(self,configfile):
         self._configfile = configfile
-        self._config = {}
-    def get_config(self,key=None):
+
+    @property
+    def config(self):
+        config = {}
         with open(self._configfile,'r') as file:
             for line in file:
                 s = line.split('=')
                 fkey = s[0].strip()
                 fvalue = s[1].strip()
-                self._config[fkey] = fvalue
-        if key == None:
-            print(self._config)
-        else:
-            print(self._config[key])
+                config[fkey] = fvalue
+        return config 
 
 
 class UserDate(object):
     def __init__(self,userdatafile):
         self._userdatafile = userdatafile
-        self._userdata = {}
-    def get_userdata(self,key=None):
+    
+    @property 
+    def userdata(self):
+        userdate = {}
         with open(self._userdatafile) as file:
             for line in file:
                 s = line.split(',')
                 fkey = s[0].strip()
                 fvalue = s[1].strip()
-                self._userdata[fkey] = fvalue
-        if key == None:
-            return self._userdate
-        else:
-            return self._userdate[key]
-
+                userdata[fkey] = fvalue
+        return userdata
 
 
 class Salary(object):
