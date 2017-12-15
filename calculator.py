355324caad854322f786cad8ff7cd967649b0d8a
hades_zhao <hades_zhao@126.com>
2017-12-15 11:05:01 +0800

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+class Config(object):
+    def __init__(self,configfile):
+        self._configfile = configfile
+        self._config = {}
+    def get_config(self,key=None):
+        with open(self._configfile,'r') as file:
+            for line in file:
+                s = line.split('=')
+                fkey = s[0].strip()
+                fvalue = s[1].strip()
+                self._config[fkey] = fvalue
+        if key == None:
+            print(self._config)
+        else:
+            print(self._config[key])
+
+
+class UserDate(object):
+    def __init__(self,userdatafile):
+        self._userdatafile = userdatafile
+        self._userdata = {}
+    def get_userdata(self,key=None):
+        with open(self._userdatafile) as file:
+            for line in file:
+                s = line.split(',')
+                fkey = s[0].strip()
+                fvalue = s[1].strip()
+                self._userdata[fkey] = fvalue
+        if key == None:
+            return self._userdate
+        else:
+            return self._userdate[key]
+    def cal_soiner(self,key=None):
+        for v in self._userdata.items()
+                       
